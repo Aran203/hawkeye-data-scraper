df5dd5dd1ec4bec3b4314dddc45bc41fc0858d00
Karan Bhalla <karanbhalla204@gmail.com>
2024-06-13 04:28:36 -0500

    Changed file structure to modularize code

--- data-scrapper.py
+++ data-scrapper.py
@@ -2,28 +2,22 @@ import requests
 import json
 import csv
 
-# Reading all match IDs from the csv file
-with open('matches/ipl-2024.csv', mode='r', newline='') as file:
-    reader = csv.reader(file)
-    header = next(reader)
-    hawkeye_ids = [row[0].strip() for row in reader]
-    match_ids = [row[1].strip() for row in reader]
+BASE_URL = "https://polls.iplt20.com/widget/welcome/get_data"
 
-def processTrajectoryData(dat: str):
-    ''' 
-        Dat is the value of a key value pair (key is trajectoryData)
-        Function converts it to a dictionary for easy access of attributes
-    '''
-    arr = dat.split("\n\n")
-    arr = [i.strip().split("\n") for i in arr]
-    arr = [[i[0][1:-1], i[1]] for i in arr]
-    myDict = {i[0]: i[1] for i in arr}
-    del myDict["Software Version"]
-    return myDict
+def fetch_bbb_data(inning, over, ball, hawkID, matchID):
+    url = f"{BASE_URL}?path=Delivery_{inning}_{over}_{ball}_{hawkID}.json"
+
+    try:
+        response = requests.get(url)
+        data = response.json()
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+        return None
+
+    if not data:
+        return []
+
+    dataArr = [matchID, inning]        
 
 
-fields = ['p_match', 'inns', 'bat', 'p_bat', 'bat_hand', 'team_bat', 'bowl', 'p_bowl', 'bowl_type', 'team_bowl', 'ball_id', 'score', 
-          'noball', 'wides', 'byes', 'leg_byes', 'six_dist', 'release_speed', 'release_x', 'release_y', 'release_z', 'bounce_angle', 
-          'bounce_x', 'bounce_y', 'crease_x', 'crease_y', 'crease_z', 'drop_angle', 'impact_x', 'impact_y', 'impact_z', 'swing', 
-          'deviation', 'swing_dist', 'ground', 'date']
 
---
+++ main.py
@@ -0,0 +1,36 @@
+import csv
+
+FIELDS = ['p_match', 'inns', 'bat', 'p_bat', 'bat_hand', 'team_bat', 'bowl', 'p_bowl', 'bowl_type', 'team_bowl', 'ball_id', 'score', 
+          'noball', 'wides', 'byes', 'leg_byes', 'six_dist', 'release_speed', 'release_x', 'release_y', 'release_z', 'bounce_angle', 
+          'bounce_x', 'bounce_y', 'crease_x', 'crease_y', 'crease_z', 'drop_angle', 'impact_x', 'impact_y', 'impact_z', 'swing', 
+          'deviation', 'swing_dist', 'ground', 'date']
+
+FILENAME = "data/ipl-2024.csv"
+
+def read_match_ids(path):
+    hawkeye_ids = []
+    match_ids = []
+
+    # Reading all match IDs from the csv file
+    with open(path, mode = 'r', newline='') as file:
+        reader = csv.reader(file)
+        header = next(reader)
+
+        for row in reader:
+            hawkeye_ids.append(row[0].strip())
+            match_ids.append(row[1].strip())
+
+    return hawkeye_ids, match_ids
+
+
+def main():
+    
+    hawkeye_ids, match_ids = read_match_ids('matches/ipl-2024.csv')
+
+    with open(FILENAME, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = FIELDS) 
+        writer.writeheader()
+
+        for i in range(len(match_ids)):
+            hawkID = hawkeye_ids[i]
+            matchID = match_ids[i]
